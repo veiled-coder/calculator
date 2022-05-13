2d4bc7a725066bdf2a830851321a531dbdf9ed4c
BintReina <omobowalerohma1998@gmail.com>
2022-05-13 06:58:08 +0100

    made changes

---
+++ script.js
@@ -0,0 +1,12 @@
+const calculator=document.querySelector(".calculator");
+const keys=calculator.querySelector(".calculator__keys")
+
+keys.addEventListener("click",e=>{
+// console.log('A key was pressed sha');
+if (e.target.matches('button')){
+ 
+}
+
+}
+
+)
