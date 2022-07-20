f356b6f7edd1f245198cadde1bf47d0f195c8a18
BintReina <omobowalerohma1998@gmail.com>
2022-07-20 13:14:51 +0100

    added the highlight effect using the :active pseudo class on the .Keys--operator

--- script.js
+++ script.js
@@ -4,61 +4,10 @@ const key1=document.querySelector(".theme--box__circle1");
 const key2=document.querySelector(".theme--box__circle2");
 const key3=document.querySelector(".theme--box__circle3");
 const operators=document.querySelectorAll('.keys--operator');
-// const operatorArray=Array.from(operators);
-// const display=document.querySelector('.display--input__text');
-// for (i=0; i<operators.length;i++){
-//    operators[i].addEventListener("mousedown",mouseDown);
-//     operators[i].addEventListener("mouseup",mouseUp);
-//     operators[i].addEventListener("keydown",keyDown);
-//     operators[i].addEventListener("keyup",keyUp);
+const display=document.querySelector(".display--input__text");
 
 
 
-// }
-
-
-
-function mouseDown(){
-
-console.log('mousedown');
-// document.documentElement.style.setProperty('--operator','var(--operator-clicked)')
-
-this.style.backgroundColor="var(--operator-clicked)";
-
-}
-
-function mouseUp(){
-// this.classList.remove('is-depressed')
-this.style.backgroundColor="var(--operator)"
-   
-// document.documentElement.style.setProperty('--operator','var(--operator)')
-
-console.log('mouseup');
-};
-
-// keys.addEventListener("click",e=>{
-
-// const key=e.target;
-// const action=key.dataset.action;
-// // action checks   the data* button atrribute matches the string "action"
-// if (e.target.matches('button')){
-//  console.log('A Key was pressed');
-// }
-//  if (!action){
-// console.log('A number key was pressed');
-// }
-//  if(action==="multiply"||action==="add"||action==="subtract"||action==="divide"){
-//    console.log("An operator key was pressed");
-   
-// } 
-// if (action==="clear"){
-//    console.log('reset button was pressed');
-// }
-// if (action ==="calculate"){
-//    console.log('the equal key was pressed');
-// };
-
-// });
 
 const setTheme = (anyTheme) => document.documentElement.className = anyTheme;
 
@@ -95,10 +44,7 @@ key2.addEventListener("click",()=>{
 keys.addEventListener('click',e=>{
      const key =e.target;
      const action=key.dataset.action;
-   //  needed to redeclare key and action variable again here because they were not declared globally previously
-   // i.e they were declared in a code block previously,since const variables are blocked scoped, variables
-   // declared in each block owns that variable,another code block cant have access to variables in other blocks.
-  const keyContent=key.textContent;
+// dataset.action checks the data* with the -action modifier in the n=btn 
   const displayedNum=display.textContent;
 
 if (!action){
@@ -115,8 +61,6 @@ display.textContent=displayedNum+"."
 }
 
 if (action === 'add' ||action === 'subtract' ||action === 'multiply' ||action === 'divide') {
-//   operators.style.backgroundColor="red";
-console.log('happy');  
+ 
 }
 })
-
